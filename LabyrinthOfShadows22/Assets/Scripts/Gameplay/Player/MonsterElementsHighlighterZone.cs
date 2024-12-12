ff71c44877b25b43453afc3d09b4997d0804de4c
RomanT <r.tarasyuta@chillgaming.net>
2024-12-13 00:55:54 +0700

    proper scripts folder strucutre

--- LabyrinthOfShadows22/Assets/Scripts/Gameplay/Player/MonsterElementsHighlighterZone.cs
+++ LabyrinthOfShadows22/Assets/Scripts/Gameplay/Player/MonsterElementsHighlighterZone.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class MonsterZoneHighlighter : MonoBehaviour
+public class MonsterElementsHighlighterZone : MonoBehaviour
 {
 
     private void OnTriggerEnter2D(Collider2D other)
