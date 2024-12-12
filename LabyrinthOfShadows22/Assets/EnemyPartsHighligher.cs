762ca1bbe6046f16afb49f818d6a0a16ad5a3cfc
RomanT <r.tarasyuta@chillgaming.net>
2024-12-13 00:14:28 +0700

    mobs highlighted in zone

---
+++ LabyrinthOfShadows22/Assets/EnemyPartsHighligher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPartsHighligher : MonoBehaviour, IHighlightable
+{
+    [SerializeField]
+    private List<GameObject> _highlightElements;
+
+    public void TurnOnHighlight()
+    {
+        foreach (var highlighGO in _highlightElements)
+        {
+            highlighGO.SetActive(true);
+        }
+    }
+
+    public void TurnOffHighlight()
+    {
+        foreach (var highlighGO in _highlightElements)
+        {
+            highlighGO.SetActive(false);
+        }
+    }
+}
