3ec5142df1130205f7ce757c5266880f6d45cc41
RomanT <r.tarasyuta@chillgaming.net>
2024-12-15 19:05:08 +0700

    new enemy type

--- LabyrinthOfShadows22/Assets/Scripts/Gameplay/Enemies/EnemyTypes/BlindEnemy.cs
+++ LabyrinthOfShadows22/Assets/Scripts/Gameplay/Enemies/EnemyTypes/BlindEnemy.cs
@@ -1,12 +1,37 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Gameplay.Enemies
 {
     public class BlindEnemy : BaseEnemy
     {
+        [SerializeField]
         private List<Transform> _patrolWayPoints;
-        
+
+        private bool hasBuiltPath = false;
+
+        protected void Start()
+        {
+            
+            //лютый костыль чтобы не разбираться с порядком вызова
+            StartCoroutine(BuildPathFromWayPointsAndSelf());
+        }
+
+        private IEnumerator BuildPathFromWayPointsAndSelf()
+        {
+            yield return new WaitForSeconds(0.5f);
+            _path = new List<Vector2Int>();
+            
+            _path.Add(_currentCell);
+            foreach (var wayPoint in _patrolWayPoints)
+            {
+                Vector2Int waypointPos = new Vector2Int((int)wayPoint.position.x, (int)wayPoint.position.y);
+                _path.Add(_gridManager.GetNode(waypointPos).Position);
+            }
+        }
+
         protected override void Update()
         {
             base.Update();
